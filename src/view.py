b02463ffc6092276f14afaaf8880d03c4e84eacc
Matheus <matheusdz155@gmail.com>
2024-12-03 11:48:24 -0500

    Created Audioview class and def init method with attributes

--- src/view.py
+++ src/view.py
@@ -3,5 +3,12 @@ from tkinter import filedialog
 import matplotlib.pyplot as plt
 import numpy as np
 
+class AudioView:
+    def __init__(self, controller):
+        self.controller = controller
+        self.root = tk.Tk()
+        self.root.title("Audio Analysis Tool")
+        self.current_band_index = 0  # Initialize current_band_index
+        self.create_widgets()
 
 
